4176d5024f898af22d1c265ae81e7e3279185039
MattScuteri <mttscuteri@gmail.com>
2018-02-02 00:46:55 -0500

    added node modules

---
+++ liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const imdb = require('omdb');
+
+// commands:
+// my-tweets - show last 20 tweets and when they were created
+// spotify-this-song - show artists, title, preview link from Spotify, album...if no song, default to "The Sign" by Ace of Base
+// movie-this - will show title, year, IMDB rating, Rotten Tomato, Country of origin, Language, Plot, Actors...if no movie, defaults to Mr Nobody
+// do-what-it-says - fs node package to read random.txt
